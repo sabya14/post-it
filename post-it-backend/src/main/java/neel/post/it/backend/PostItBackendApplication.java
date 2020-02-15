9ee24b12566f3b4cf2f619fdc9854d2190339af3
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-02-15 14:52:01 +0530

    Initial Commit

---
+++ post-it-backend/src/main/java/neel/post/it/backend/PostItBackendApplication.java
@@ -0,0 +1,13 @@
+package neel.post.it.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostItBackendApplication.class, args);
+    }
+
+}
---
+++ post-it-backend/src/test/java/neel/post/it/backend/PostItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package neel.post.it.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
